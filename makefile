f0f94fb44069f8be5f80e2146ce41219785302bb
Hilary Zen <hzen00@stuy.edu>
2019-11-13 11:00:06 -0500

    Added

---
+++ makefile
@@ -0,0 +1,5 @@
+all: stat.o
+	gcc -o program stat.o
+
+stat.o: stat.c
+	gcc -c stat.c
--- stat.c
+++ stat.c
@@ -1,3 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
+
+int main() {
+  struct stat *s;
+  stat("makefile", s);
+  printf("File size: %ld\n", s->st_size);
+  printf("Mode (permissions): \n");
+  printf("Time of last access: \n");
+  return 0;
+}
