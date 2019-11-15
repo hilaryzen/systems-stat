ec49eeae872a2a0938059b3b0f7dc8adc1b3deea
Hilary Zen <hzen00@stuy.edu>
2019-11-15 00:15:01 -0500

    Added neat bytes

--- stat.c
+++ stat.c
@@ -8,8 +8,18 @@ int main() {
   struct stat s;
   stat("./makefile", &s);
   int bytes = s.st_size;
-  printf("File size: %d\n", bytes);
-  printf("Mode (permissions): %d\n", s.st_mode);
+  char * end[] = {"B", "KB", "MB", "GB"};
+  int endI = 0;
+  while (bytes >= 1024 && endI < 3) {
+    bytes = bytes / 1024;
+    endI += 1;
+  }
+  char bytesR [10];
+  int n = sprintf(bytesR, "%d %s", bytes, end[endI]);
+  printf("File size: %s\n", bytesR);
+
+  printf("Mode (permissions): %o\n", s.st_mode % 4096);
+
   time_t time = s.st_atime;
   printf("Time of last access: %s\n", ctime(&time));
   return 0;
