d2e0e3d6bc62b47c42ee0876658b918ab318f791
Hilary Zen <hzen00@stuy.edu>
2019-11-14 23:23:14 -0500

    Finished first part

--- stat.c
+++ stat.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/stat.h>
+#include <time.h>
 
 int main() {
-  struct stat *s;
-  stat("makefile", s);
-  printf("File size: %ld\n", s->st_size);
-  printf("Mode (permissions): \n");
-  printf("Time of last access: \n");
+  struct stat s;
+  stat("./makefile", &s);
+  int bytes = s.st_size;
+  printf("File size: %d\n", bytes);
+  printf("Mode (permissions): %d\n", s.st_mode);
+  time_t time = s.st_atime;
+  printf("Time of last access: %s\n", ctime(&time));
   return 0;
 }
